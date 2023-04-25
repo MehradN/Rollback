72832c7843f938b75ac90a7e9330ba412c412e40
Mehrad Nayyeri <mehrad.nayyeri@zohomail.com>
2023-04-25 16:37:43 +0330

    A fresh new start
    The project is going to change drastically.
    So I've decided to reset back to the template for easier development.

---
+++ src/client/java/ir/mehradn/rollback/RollbackClient.java
@@ -0,0 +1,8 @@
+package ir.mehradn.rollback;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RollbackClient implements ClientModInitializer {
+    public void onInitializeClient() {
+    }
+}
--- src/main/java/ir/mehradn/rollback/Rollback.java
+++ src/main/java/ir/mehradn/rollback/Rollback.java
@@ -1,24 +1,13 @@
 package ir.mehradn.rollback;
 
-import ir.mehradn.rollback.config.RollbackConfig;
-import ir.mehradn.rollback.event.AutomatedBackup;
-import ir.mehradn.rollback.event.RollbackCommand;
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
+import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Environment(EnvType.CLIENT)
-public class Rollback implements ClientModInitializer {
+public class Rollback implements ModInitializer {
     public static final String MOD_ID = "rollback";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public void onInitializeClient() {
-        LOGGER.info("Registering MidnightConfig...");
-        RollbackConfig.register();
-        LOGGER.info("Registering events...");
-        AutomatedBackup.register();
-        RollbackCommand.register();
+    public void onInitialize() {
     }
 }
