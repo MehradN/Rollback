51568b0599f78c8996c3e36581218769e77304b0
Mehrad Nayyeri <mehrad.nayyeri@zohomail.com>
2025-01-31 17:51:56 +0330

    Build the project structure

---
+++ src/client/java/ir/mehradn/rollback/RollbackClient.java
@@ -0,0 +1,9 @@
+package ir.mehradn.rollback;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RollbackClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/ir/mehradn/rollback/Rollback.java
@@ -0,0 +1,14 @@
+package ir.mehradn.rollback;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Rollback implements ModInitializer {
+    public static final String MOD_ID = "rollback";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+    }
+}
